33ed156e44695f9847e12b18d10afc100e19f76e
Atila Santos <atila.romao@hotmail.com>
2021-11-09 04:27:33 -0500

    First sample code with golang programming language

---
+++ hello.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+
+	"rsc.io/quote"
+)
+
+func main() {
+	fmt.Println(quote.Hello())
+	fmt.Println("N1 + N2 = ", sum(10, 20))
+}
+
+func sum(n1 int, n2 int) int {
+	return n1 + n2
+}
