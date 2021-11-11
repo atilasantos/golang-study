e415e6c3312822b908e6121add56f3855b402b63
Atila romao <ars@lionx.com.br>
2021-11-11 19:32:27 -0300

    Adding log reading and writing

--- hello.go
+++ hello.go
@@ -4,18 +4,17 @@ import (
 	"bufio"
 	"fmt"
 	"io"
-	"log"
+	"io/ioutil"
 	req "net/http"
 	"os"
 	"strings"
 	"time"
 )
 
-const monitorCicle = 3
+const monitorCicle = 4
 const waitCicleTime = 4
 
 func main() {
-	fmt.Println(returnSitesFromFile())
 	showIntroduction()
 	for {
 		showMenu(getUserOption())
@@ -45,9 +44,10 @@ func showMenu(option int) {
 	switch option {
 	case 1:
 		fmt.Println("Tá tudo funfando")
-		checkWebsites(sitesToMonitor())
+		checkWebsites(returnSitesFromFile())
 	case 2:
 		fmt.Println("Oia os log")
+		displayLogs()
 	case 0:
 		fmt.Println("Exiting...")
 		os.Exit(0)
@@ -58,6 +58,7 @@ func showMenu(option int) {
 }
 
 func checkWebsites(sites []string) {
+	fmt.Println(sites)
 	for i := 0; i < monitorCicle; i++ {
 		for _, site := range sites {
 			isSiteUp(site)
@@ -68,20 +69,17 @@ func checkWebsites(sites []string) {
 
 }
 
-func sitesToMonitor() []string {
-	sitesToMonitor := []string{"http://www.google.com", "http://www.alura.com.br", "http://www.facebook.com"}
-	return sitesToMonitor
-}
-
 func isSiteUp(site string) {
 	response, error := req.Get(site)
 	if error != nil {
-		log.Fatalln("Error:", error)
+		fmt.Println("An error ocurred: ", error)
 	}
 	if response.StatusCode == 200 {
 		fmt.Printf("Site %s carregado com status %d de sucesso!\n", site, response.StatusCode)
+		logSiteStatus(site, true)
 	} else {
 		fmt.Printf("Site %s carregado com status %d de erro!\n", site, response.StatusCode)
+		logSiteStatus(site, false)
 	}
 }
 
@@ -91,18 +89,44 @@ func returnSitesFromFile() []string {
 
 	file, error := os.Open("./sites.txt")
 	if error != nil {
-		log.Fatalln("Error:", error)
+		fmt.Println("An error ocurred: ", error)
 	}
 
 	reader := bufio.NewReader(file)
 	for {
 		readLine, err := reader.ReadString('\n')
-		readLine = strings.Trim(readLine, "\n")
+		readLine = strings.TrimSpace(readLine)
+		sites = append(sites, readLine)
 		if err == io.EOF {
 			break
 		}
-		sites = append(sites, readLine)
 	}
 	file.Close()
 	return sites
+}
+
+func logSiteStatus(site string, isUp bool) {
+	file, err := os.OpenFile("logs.txt", os.O_CREATE|os.O_RDWR|os.O_APPEND, 0666)
+
+	if err != nil {
+		fmt.Println("Ocorreu um erro.", err)
+	}
+
+	if isUp {
+		file.WriteString(time.Now().Format("02/01/2006 - 15:04:05") + " - [OK] - Site - " + site + " - available.")
+		file.WriteString("\n")
+	} else {
+		file.WriteString(time.Now().Format("02/01/2006 - 15:04:05") + " - [NOK] - Site - " + site + " - Not available")
+		file.WriteString("\n")
+	}
+	file.Close()
+}
+
+func displayLogs() {
+	file, error := ioutil.ReadFile("./logs.txt")
+	if error != nil {
+		fmt.Println("An error ocurred: ", error)
+	}
+
+	fmt.Println(string(file))
 }
